7006aa56bd499993ef03bacc1482d41f4b06c20b
loveneesh bhatnagar <loveneesh1716.cse18@chitkara.edu.in>
2021-02-21 17:04:15 +0530

    Create source_code.py

---
+++ source_code.py
@@ -0,0 +1,20 @@
+'''
+Convert any images to ASCII art using Python
+'''
+
+#import the necessary module!
+import pywhatkit as kt
+
+print("Let's convert images to ASCII art!")
+
+#Input source path 
+print("Enter file name to convert")
+source_path = input()
+
+#target_path = 'asciify.text'
+target_path = 'asciify.text'
+
+
+#call the method
+kt.image_to_ascii_art(source_path, target_path)
+
